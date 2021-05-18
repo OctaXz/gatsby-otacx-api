ec0a8b7cce54dbfa897885f12fe56e39a615ad83
OctaXAdmin <octax.bsc@hotmail.com>
2021-05-18 22:39:19 +0700

    --firt commit

---
+++ utils/constants.ts
@@ -0,0 +1,19 @@
+import { ChainId, Token } from "@pancakeswap-libs/sdk";
+
+// BEP-20 addresses.
+export const CAKE = "0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82";
+export const WBNB = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";
+export const DEAD = "0x000000000000000000000000000000000000dEaD";
+
+// Contract addresses.
+export const CAKE_BNB_FARM = "0xA527a61703D82139F8a06Bc30097cC9CAA2df5A6";
+export const MASTERCHEF_CONTRACT = "0x73feaa1eE314F8c655E354234017bE2193C9E24E";
+export const LOTTERY_CONTRACT = "0x55136140069867cdb093240CC3144fdc298FeC90";
+export const MULTICALL_CONTRACT = "0x1Ee38d535d541c55C9dae27B12edf090C608E6Fb";
+
+// PancakeSwap SDK Token.
+export const CAKE_TOKEN = new Token(ChainId.MAINNET, CAKE, 18);
+export const WBNB_TOKEN = new Token(ChainId.MAINNET, WBNB, 18);
+export const CAKE_BNB_TOKEN = new Token(ChainId.MAINNET, CAKE_BNB_FARM, 18);
+
+
