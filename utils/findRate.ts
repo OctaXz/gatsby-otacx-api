acab13ced3379eff40c9a395aeddc23a4907999e
OctaXAdmin <octax.bsc@hotmail.com>
2021-06-11 15:26:37 +0700

    --edit lottery contract

--- utils/findRate.ts
+++ utils/findRate.ts
@@ -6,10 +6,10 @@ const routerAdress = '0x10ed43c718714eb63d5aa57b78b54704e256024e'
 const contract = getContract(router, routerAdress);
 
 export const getAmountsIn = async (amount:number,pairs:Array<string>): Promise<BigNumber> => {
-  const amountMul = new BigNumber(amount).times(1e18).toNumber()
-  console.log(amount,amountMul,pairs)
-
-  const [amountIn,amountOut] = await contract.methods.getAmountsIn(amountMul,pairs).call();
+  // const amountMul = new BigNumber(amount).times(1e18)
+  // console.log(amount,amountMul,pairs)
+ console.log(amount,pairs)
+  const [amountIn,amountOut] = await contract.methods.getAmountsIn(amount,pairs).call();
 
   return new BigNumber(amountOut);
 };
